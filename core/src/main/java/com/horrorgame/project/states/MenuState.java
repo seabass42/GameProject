37ad9aff0efe4cc0d92c839970b8d4dff1522b89
Sebastian <sebastian.ezpeleta-stewart@sjsu.edu>
2025-10-13 23:36:09 -0700

    First commit

---
+++ core/src/main/java/com/horrorgame/project/states/MenuState.java
@@ -0,0 +1,9 @@
+package com.horrorgame.project.states;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class MenuState {
+    private Texture background;
+    private Texture button1, button2;
+
+}
