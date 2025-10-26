585cf8663bb29fff4ec9bdaef26101386d1fa3de
Sebastian <sebastian.ezpeleta-stewart@sjsu.edu>
2025-10-26 16:33:54 -0700

    Fixed

---
+++ core/src/main/java/com/horrorgame/project/states/GameStateManager.java
@@ -0,0 +1,28 @@
+package com.horrorgame.project.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+public class GameStateManager {
+    private Stack<State> states;
+
+    public GameStateManager(){
+        states = new Stack<State>();
+    }
+
+    public void push(State state){
+        states.push(state);
+    }
+    public void pop(){
+        states.pop();
+    }
+
+    public void update(float dt){
+        states.peek().update(dt);
+    }
+
+    public void render(SpriteBatch sb){
+        states.peek().render(sb);
+    }
+}
