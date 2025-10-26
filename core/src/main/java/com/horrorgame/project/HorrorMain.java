ce0c8bb1865d27ae85bf5cc137a3e369fa4223eb
Sebastian <sebastian.ezpeleta-stewart@sjsu.edu>
2025-10-26 16:12:57 -0700

    Update

--- core/src/main/java/com/horrorgame/project/HorrorMain.java
+++ core/src/main/java/com/horrorgame/project/HorrorMain.java
@@ -6,29 +6,38 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.horrorgame.project.states.GameStateManager;
+import com.horrorgame.project.states.MenuState;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class HorrorMain extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private GameStateManager gsm;
+
+    public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1080;
+    public static final String TITLE = "";
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        gsm = new GameStateManager();
+        //ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new MenuState(gsm));
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.update(Gdx.graphics.getDeltaTime());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gsm.render(batch);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+
     }
 }
--- core/src/main/java/com/horrorgame/project/states/MenuState.java
+++ core/src/main/java/com/horrorgame/project/states/MenuState.java
@@ -1,9 +1,45 @@
 package com.horrorgame.project.states;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.horrorgame.project.HorrorMain;
 
-public class MenuState {
+public class MenuState extends State {
     private Texture background;
-    private Texture button1, button2;
+    private Texture playBtn, exitBtn;
+    private Stage stage;
+    private Skin skin;
 
+    public MenuState(GameStateManager gsm){
+        super(gsm);
+        background = new Texture("menu.jpeg");
+        //playBtn = new Texture("enter image here");
+        //exitBtn = new Texture("enter image here");
+        stage = new Stage(new ScreenViewport());
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background,0,0, HorrorMain.WIDTH / 1.2f, HorrorMain.HEIGHT/ 1.2f);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
