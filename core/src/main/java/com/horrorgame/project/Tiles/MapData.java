40d926a44f669d750d34861ceb6c92ed9acf0a79
Sebastian <sebastian.ezpeleta-stewart@sjsu.edu>
2025-11-08 02:27:53 -0800

    Mapdata moved to branch and width/height changed

--- core/src/main/java/com/horrorgame/project/Tiles/MapData.java
+++
@@ -1,8 +0,0 @@
-package com.horrorgame.project.Tiles;
-
-public class MapData {
-    public static int[][] gameStateMap =
-        {{23,23,23,23,23}, {23,23,23,23,23}, {23,23,23,23,23},
-        {23,23,23,23,23}, {23,23,23,23,23}, {23,23,23,23,23}, {23,23,23,23,23}};
-
-    }
