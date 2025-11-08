6ed37a8b33573284c0dd91fe52a9ee9415974905
Sebastian <sebastian.ezpeleta-stewart@sjsu.edu>
2025-11-08 02:14:28 -0800

    MapData Class

---
+++ core/src/main/java/com/horrorgame/project/Tiles/MapData.java
@@ -0,0 +1,8 @@
+package com.horrorgame.project.Tiles;
+
+public class MapData {
+    public static int[][] gameStateMap =
+        {{23,23,23,23,23}, {23,23,23,23,23}, {23,23,23,23,23},
+        {23,23,23,23,23}, {23,23,23,23,23}, {23,23,23,23,23}, {23,23,23,23,23}};
+
+    }
